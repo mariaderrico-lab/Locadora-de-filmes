9466d31176f76a539509a43d9b4aad31eba44124
mariaderrico-lab <maria.d'errico@ba.estudante.senai.br>
2026-09-16 19:52:07 -0300

    algumas atualizações

--- Model/Filme.php
+++ Model/Filme.php
@@ -1,10 +1,14 @@
 <?php
+
 namespace Model;
-require_once __DIR__ . "/connection.php";
+
+require_once __DIR__ . "Connection.php";
+
+use PDO;
 
 class Filme
 {
-   private $conn;
+    private $conn;
 
     public function __construct()
     {
@@ -13,22 +17,27 @@ class Filme
 
     public function listar()
     {
-        $stmt = $this->conn->prepare("SELECT * FROM filmes ORDER BY id DESC");
+        $stmt = $this->conn->prepare(
+            "SELECT * FROM filmes ORDER BY id DESC"
+        );
+
         $stmt->execute();
 
-        return $stmt->fetchAll(\PDO::FETCH_ASSOC);
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 
     public function cadastrar($titulo, $genero, $disponivel = 1)
     {
         $stmt = $this->conn->prepare(
-            "INSERT INTO filmes (titulo, genero, disponivel, preco)
-             VALUES (:titulo, :genero, :disponivel, 10.00)"
+            "INSERT INTO filmes 
+            (titulo, genero, disponivel, preco)
+            VALUES 
+            (:titulo, :genero, :disponivel, 10.00)"
         );
 
         $stmt->bindParam(":titulo", $titulo);
         $stmt->bindParam(":genero", $genero);
-        $stmt->bindParam(":disponivel", $disponivel, \PDO::PARAM_INT);
+        $stmt->bindParam(":disponivel", $disponivel, PDO::PARAM_INT);
 
         return $stmt->execute();
     }
@@ -39,7 +48,7 @@ class Filme
             "DELETE FROM filmes WHERE id = :id"
         );
 
-        $stmt->bindParam(":id", $id, \PDO::PARAM_INT);
+        $stmt->bindParam(":id", $id, PDO::PARAM_INT);
 
         return $stmt->execute();
     }
